487035cbf69f4544863d391d9cdd4ca2f202ab89
ogyrec <ogyrec.off@gmail.com>
2026-02-15 20:46:13 +0100

    refactor: flatten connectors/ and tasks/ packages

--- src/rune_companion/tasks/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Task subsystem.
-
-Components:
-- task_models.py: data structures (Task, TaskStatus)
-- task_store.py: SQLite-backed storage + query/update helpers
-- task_scheduler.py: polling scheduler that dispatches runnable tasks
-- task_api.py: small high-level helpers used by the rest of the app
-"""
